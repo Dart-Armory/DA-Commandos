a167cc92dfeb37b3c4a7666f88aedfcd749e7905
William Grate <skulldude327@gmail.com>
2023-05-25 22:00:40 -0500

    Fixed extra slashes in file paths

--- DAC_Armors/Imperial/Config.cpp
+++ DAC_Armors/Imperial/Config.cpp
@@ -27,8 +27,8 @@ class CfgWeapons
 		displayName = "[DA] Katarn I Imperial Commando Helmet";
 		hiddenSelectionsTextures[] =
 		{
-			DAC_Armors\Imperial\Data\\Helmets\Imperial_Helmet.paa, // Visor
-			DAC_Armors\Imperial\Data\\Helmets\Imperial_Helmet.paa  // Main Helmet
+			DAC_Armors\Imperial\Data\Helmets\Imperial_Helmet.paa, // Visor
+			DAC_Armors\Imperial\Data\Helmets\Imperial_Helmet.paa  // Main Helmet
 		};
 	};
 	// ┌────────────────────┐
@@ -69,8 +69,8 @@ class CfgVehicles
 		uniformClass = "DAC_Uniform_Imperial_Commando";
 		hiddenSelectionsTextures[] =
 		{
-			DAC_Armors\Imperial\Data\\Uniforms\Imperial_Uniform_Upper.paa,
-			DAC_Armors\Imperial\Data\\Uniforms\Imperial_Uniform_Lower.paa
+			DAC_Armors\Imperial\Data\Uniforms\Imperial_Uniform_Upper.paa,
+			DAC_Armors\Imperial\Data\Uniforms\Imperial_Uniform_Lower.paa
 		};
 		modelSides[] = {0, 1, 2, 3}; // Makes the model available to all sides
 
