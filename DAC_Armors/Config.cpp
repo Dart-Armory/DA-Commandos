91606b3335429ac6cadabd41011d8071152aa07d
DartRuffian <skulldude327@gmail.com>
2023-09-06 15:44:16 -0500

    Simplified inheritance

--- DAC_Armors/Config.cpp
+++ DAC_Armors/Config.cpp
@@ -28,7 +28,7 @@ class CfgFactionClasses
 		// Mod Info
         dlc = "DA_Commando_Mod";
         author = "DartRuffian";
-		
+
 		// Scope
         scope = 2;
         scopeCurator = 2;
@@ -36,17 +36,8 @@ class CfgFactionClasses
         displayName = "[DA] Commando Squads";
     };
 
-	class DA_Commandos_OPFOR
+	class DA_Commandos_OPFOR: DA_Commandos
     {
-		// Mod Info
-        dlc = "DA_Commando_Mod";
-        author = "DartRuffian";
-		
-		// Scope
-        scope = 2;
-        scopeCurator = 2;
-		
-        displayName = "[DA] Commando Squads";
 		side = 0;
     };
 };
