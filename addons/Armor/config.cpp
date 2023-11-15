2d66cb628e7bdc9c8169f709b8f93044a091f11d
DartRuffian <skulldude327@gmail.com>
2023-11-14 23:09:55 -0600

    Fixed CfgPatches

--- addons/Armor/config.cpp
+++ addons/Armor/config.cpp
@@ -23,7 +23,42 @@ class CfgPatches
             "SWLB_clones_spec_backpacks"
                 // Commando Backpacks
         };
-        units[] = {};
-        weapons[] = {};
+        units[] =
+        {
+            // Units
+            "DAC_Unit_Commando_Base",
+            "DAC_Unit_Commando_SL_Base",
+            "DAC_Unit_Commando_EOD_Base",
+            "DAC_Unit_Commando_DM_Base",
+            "DAC_Unit_Commando_Tech_Base",
+
+            // Backpacks
+            "DAC_Backpack_Base",
+            "DAC_Backpack_Base_Huge",
+            "DAC_Backpack_RTO_Base",
+            "DAC_Backpack_RTO_Base_Huge",
+            "DAC_Backpack_EOD_Base",
+            "DAC_Backpack_EOD_Base_Huge",
+            "DAC_Backpack_Tech_Base",
+            "DAC_Backpack_Tech_Base_Huge",
+
+            // Placeable Helmets
+            "DAC_HelmetHolder_Commando_Base"
+        };
+        weapons[] =
+        {
+            // Helmets
+            "DAC_Helmet_Commando_Base",
+
+            // Uniforms
+            "DAC_Uniform_Commando_Base",
+
+            // Vests
+            "DAC_Vest_Commando_Base",
+            "DAC_Vest_Commando_SL_Base",
+            "DAC_Vest_Commando_EOD_Base",
+            "DAC_Vest_Commando_DM_Base",
+            "DAC_Vest_Commando_Tech_Base"
+        };
     };
 };
