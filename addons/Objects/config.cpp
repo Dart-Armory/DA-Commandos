fb9a746a82e8d1fab99e61def03320746a2cff72
DartRuffian <skulldude327@gmail.com>
2023-11-15 12:23:30 -0600

    Base objects addon

---
+++ addons/Objects/config.cpp
@@ -0,0 +1,23 @@
+#include "script_component.hpp"
+
+
+class CfgPatches
+{
+    class ADDON
+    {
+        author = "DartRuffian";
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] =
+        {
+            "A3_Data_F_Decade_Loadorder",
+            "DAC_Core",
+            "DAC_Armor",
+            "DAC_Customs"
+        };
+        units[] =
+        {
+
+        };
+        weapons[] = {};
+    };
+};
---
+++ addons/Objects/script_component.hpp
@@ -0,0 +1,5 @@
+#define COMPONENT Objects
+
+#include "\DA\DAC\Core\script_mod.hpp"
+#include "\DA\DAC\Core\script_macros.hpp"
+#include "\DA\DAC\Objects\script_macros.hpp"
