981a69f3a52d92811433ddbc90dfd61bc4a33281
DartRuffian <skulldude327@gmail.com>
2023-11-15 22:37:47 -0600

    Fixed CfgGroups

--- addons/Armor/CfgGroups.hpp
+++ addons/Armor/CfgGroups.hpp
@@ -1,68 +1,65 @@
 class CfgGroups
 {
-    class CfgGroups
+    class West
     {
-        class West
+        class DAC_SOB
         {
-            class DAC_SOB
+            name = "[DA] Special Operations Brigade";
+
+            class DAC_Commandos_Base
             {
-                name = "[DA] Special Operations Brigade";
-
-                class DAC_Commandos_Base
+                name = "Commando Squads";
+                class Base
                 {
-                    name = "Commando Squads";
-                    class Base
-                    {
-                        faction = "DAC_SOB";
-                        name = "Base Squad";
-                        icon = "\A3\ui_f\data\map\markers\nato\b_inf.paa";
-                        side = BLUFOR;
+                    faction = "DAC_SOB";
+                    name = "Base Squad";
+                    icon = "\A3\ui_f\data\map\markers\nato\b_inf.paa";
+                    side = BLUFOR;
 
-                        class Object0
-                        {
-                            side = BLUFOR;
-                            vehicle = "DAC_Unit_Commando_SL_Base";
-                            rank = "SERGEANT";
-                            position[] = {0, 0, 0};
-                            dir = 0;
-                        };
-                        class Object1: Object0
-                        {
-                            vehicle = "DAC_Unit_Commando_EOD_Base";
-                            rank = "CORPORAL";
-                            position[] = {1, -1, 0};
-                        };
-                        class Object2: Object1
-                        {
-                            vehicle = "DAC_Unit_Commando_Tech_Base";
-                            position[] = {-1, -1, 0};
-                        };
-                        class Object3: Object1
-                        {
-                            vehicle = "DAC_Unit_Commando_DM_Base";
-                            position[] = {2, -2, 0};
-                        };
+                    class Object0
+                    {
+                        side = BLUFOR;
+                        vehicle = "DAC_Unit_Commando_SL_Base";
+                        rank = "SERGEANT";
+                        position[] = {0, 0, 0};
+                        dir = 0;
+                    };
+                    class Object1: Object0
+                    {
+                        vehicle = "DAC_Unit_Commando_EOD_Base";
+                        rank = "CORPORAL";
+                        position[] = {1, -1, 0};
+                    };
+                    class Object2: Object1
+                    {
+                        vehicle = "DAC_Unit_Commando_Tech_Base";
+                        position[] = {-1, -1, 0};
+                    };
+                    class Object3: Object1
+                    {
+                        vehicle = "DAC_Unit_Commando_DM_Base";
+                        position[] = {2, -2, 0};
                     };
                 };
+            };
 
-                class DAC_Commandos_Imperial: DAC_Commandos_Base
+            class DAC_Commandos_Imperial: DAC_Commandos_Base
+            {
+                class Object0: Object0
                 {
-                    class Object0: Object0
-                    {
-                        vehicle = "DAC_Unit_Imperial_Commando";
-                    };
-                    class Object1: Object1
-                    {
-                        vehicle = "DAC_Unit_Imperial_Commando_EOD";
-                    };
-                    class Object2: Object2
-                    {
-                        vehicle = "DAC_Unit_Imperial_Commando_Tech";
-                    };
-                    class Object3: Object3
-                    {
-                        vehicle = "DAC_Unit_Imperial_Commando_DM";
-                    };
+                    vehicle = "DAC_Unit_Imperial_Commando";
+                };
+                class Object1: Object1
+                {
+                    vehicle = "DAC_Unit_Imperial_Commando_EOD";
+                };
+                class Object2: Object2
+                {
+                    vehicle = "DAC_Unit_Imperial_Commando_Tech";
+                };
+                class Object3: Object3
+                {
+                    vehicle = "DAC_Unit_Imperial_Commando_DM";
                 };
             };
         };
