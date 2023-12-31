b0cdfa3fa239b5838c720f4804fc6f588d370a77
DartRuffian <skulldude327@gmail.com>
2023-12-30 21:57:26 -0600

    Updated macros

--- addons/core/script_macros.hpp
+++ addons/core/script_macros.hpp
@@ -1,4 +1,53 @@
+#ifdef __A3_DEBUG__
+    #include "\DA\DAC\addons\core\script_debug.hpp"
+#endif
 #include "\x\cba\addons\main\script_macros_common.hpp"
+
+// Functions
+#undef PREP
+#ifdef DISABLE_COMPILE_CACHE
+    #define PREP(fncName) DFUNC(fncName) = compile preprocessFileLineNumbers QPATHTOF(functions\DOUBLES(fnc,fncName).sqf)
+#else
+    #define PREP(fncName) [QPATHTOF(functions\DOUBLES(fnc,fncName).sqf), QFUNC(fncName)] call CBA_fnc_compileFunction
+#endif
+
+#define CLASS(var1) DOUBLES(PREFIX,var1)
+#define QCLASS(var1) QUOTE(CLASS(var1))
+
+#define GETVAR_SYS(var1,var2) getVariable [ARR_2(QUOTE(var1),var2)]
+#define SETVAR_SYS(var1,var2) setVariable [ARR_2(QUOTE(var1),var2)]
+#define SETPVAR_SYS(var1,var2) setVariable [ARR_3(QUOTE(var1),var2,true)]
+
+#undef GETVAR
+#define GETVAR(var1,var2,var3) (var1 GETVAR_SYS(var2,var3))
+#define GETMVAR(var1,var2) (missionNamespace GETVAR_SYS(var1,var2))
+#define GETUVAR(var1,var2) (uiNamespace GETVAR_SYS(var1,var2))
+#define GETPRVAR(var1,var2) (profileNamespace GETVAR_SYS(var1,var2))
+#define GETPAVAR(var1,var2) (parsingNamespace GETVAR_SYS(var1,var2))
+
+#undef SETVAR
+#define SETVAR(var1,var2,var3) var1 SETVAR_SYS(var2,var3)
+#define SETPVAR(var1,var2,var3) var1 SETPVAR_SYS(var2,var3)
+#define SETMVAR(var1,var2) missionNamespace SETVAR_SYS(var1,var2)
+#define SETUVAR(var1,var2) uiNamespace SETVAR_SYS(var1,var2)
+#define SETPRVAR(var1,var2) profileNamespace SETVAR_SYS(var1,var2)
+#define SETPAVAR(var1,var2) parsingNamespace SETVAR_SYS(var1,var2)
+
+#define GETGVAR(var1,var2) GETMVAR(GVAR(var1),var2)
+#define GETEGVAR(var1,var2,var3) GETMVAR(EGVAR(var1,var2),var3)
+
+#define EDITOR_PREVIEW(var1) QPATHTOF(data\previews\CLASS(var1).jpg)
+#define EEDITOR_PREVIEW(var1,var2) QUOTE(PATHTOF_SYS(PREFIX,var1,data\previews\CLASS(var2).jpg))
+
+#define SCOPE_PUBLIC scope = 2; \
+scopeArsenal = 2; \
+scopeCurator = 2
+#define SCOPE_HIDDEN scope = 1; \
+scopeArsenal = 0; \
+scopeCurator = 0
+#define SCOPE_PRIVATE scope = 0; \
+scopeArsenal = 0; \
+scopeCurator = 0
 
 // Inventory & Loadouts
 #define ITEM_2(a) a, a
@@ -54,6 +103,6 @@
 #define RHOT 4
 #define RBHOT 5
 
-// Scripting
+// Booleans
 #define TRUE 1
 #define FALSE 0
