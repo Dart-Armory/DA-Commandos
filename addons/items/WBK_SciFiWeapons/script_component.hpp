090c2f872fc8dc19463960f8ed8fba97d61063d6
DartRuffian <skulldude327@gmail.com>
2023-12-31 00:17:08 -0600

    Moved commando knife to weapons

--- addons/items/WBK_SciFiWeapons/script_component.hpp
+++
@@ -1,4 +0,0 @@
-#define SUBCOMPONENT wbk_sciFiWeaps
-#define SUBCOMPONENT_BEAUTIFIED WBK Sci-Fi Weapons
-
-#include "..\script_component.hpp"
--- addons/items/script_component.hpp
+++
@@ -1,5 +0,0 @@
-#define COMPONENT items
-#define COMPONENT_BEAUTIFIED Items
-
-#include "\DA\DAC\addons\core\script_mod.hpp"
-#include "\DA\DAC\addons\core\script_macros.hpp"
